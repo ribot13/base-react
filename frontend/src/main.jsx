560d5691f03c49017a2bad36fcb5a454559aba33
Marawa Handmade <marawa.handmade@gmail.com>
2025-12-02 19:01:22 +0700

    clean css

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -3,8 +3,12 @@ import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 
 // Import CSS global Anda (jika ada)
+import './styles/theme.css'; 
 import './styles/global.css'; 
 import "./styles/LoginPage.css";
+import './index.css';
+import './styles/components/ui-components.css';
+
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
